66c7bb908b6c7e43ec1b334f718d8afd1a6c7bd2
yozica <lichengmingisme@foxmail.com>
2024-10-07 16:23:09 +0800

    feat: 添加了观影室信息的类型

--- src/types/chat.d.ts
+++ src/types/chat.d.ts
@@ -3,7 +3,7 @@ export type ChatCacheType = {
   id: number;
   clientId: string;
   nickname: string;
-  type: 'text' | 'userAction';
+  type: 'text' | 'userAction' | 'videoAction';
   data: string;
   time: string;
 };
